302f101d196195ac1671e912a4ee6d4f01af36f0
SofTest94 <165555006+SofTest94@users.noreply.github.com>
2024-05-13 01:53:50 -0600

    Delete src directory

--- src/utils/functions.ts
+++
@@ -1,9 +0,0 @@
-export const handleScheduleAppointmentClick = (telephone: string, message: string) => {
-  const msg = encodeURIComponent(message);
-  const url = `https://wa.me/${telephone}?text=${msg}`;
-  window.open(url, '_blank');
-};
-
-export const handleSocialMediaClick = (url: string) => {
-  window.open(url, '_blank');
-};
